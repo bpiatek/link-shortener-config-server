71dc7e2b3e7152e06f5f697e2d0bc1de89031b04
Bartosz Piatek <your.email@example.com>
2025-08-21 14:19:13 +0200

    initial commit

---
+++ src/test/java/pl/bpiatek/linkshortenerconfigserver/LinkShortenerConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bpiatek.linkshortenerconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
